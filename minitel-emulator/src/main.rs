c7be8b828cdb76f71b7b884ef56c823d7d6959b8
mbenoukaiss <mbenoukaiss@gmail.com>
2026-04-03 21:40:55 +0200

    Setup base bevy project

--- minitel-emulator/src/main.rs
+++ minitel-emulator/src/main.rs
@@ -1,3 +1,21 @@
+use bevy::prelude::*;
+use bevy::window::WindowResolution;
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(ClearColor(Color::BLACK))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: String::from("Minitel Emulator"),
+                resolution: WindowResolution::new(960, 600),
+                ..Default::default()
+            }),
+            ..Default::default()
+        }))
+        .add_systems(Startup, setup)
+        .run();
 }
